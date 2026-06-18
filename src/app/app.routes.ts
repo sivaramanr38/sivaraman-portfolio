9aec01c0dd87d10cd28e8c29feacf756c8151d0c
sivaramanr38 <sivaramanr38@gmail.com>
2026-06-18 21:51:58 +0530

    Update landing page with basic details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './landing-page/landing-page';
+import { Skills } from './skills/skills';
+import { Experience } from './experience/experience';
+import { Projects } from './projects/projects';
+import { Certifications } from './certifications/certifications';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: LandingPage
+    },
+    {
+        path: 'skills',
+        component: Skills
+    },
+    {
+        path: 'experience',
+        component: Experience
+    },
+    {
+        path: 'projects',
+        component: Projects
+    },
+    {
+        path: 'certifications',
+        component: Certifications
+    }
+];
---
+++ src/app/skills/skills.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-skills',
+  imports: [RouterModule],
+  templateUrl: './skills.html',
+  styleUrl: './skills.css',
+})
+export class Skills {}
